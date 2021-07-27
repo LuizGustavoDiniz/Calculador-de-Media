3735c520847422706fc12a217ca59b203aa4d73c
Luiz Gustavo Gonçalves Diniz <gustavo.diniz.gon@gmail.com>
2021-07-27 19:18:42 -0300

    inserindo map

--- app.js
+++ app.js
@@ -11,20 +11,26 @@ averageCalculatorForm.addEventListener('submit', event => {
   const referenceYear = averageCalculatorForm.year.value
   
 
-  const grades = [
-    Number(averageCalculatorForm.nota1.value),
-    Number(averageCalculatorForm.nota2.value),
-    Number(averageCalculatorForm.nota3.value),
-    Number(averageCalculatorForm.nota4.value)
-  ]
+  // const grades = [
+  //   Number(averageCalculatorForm.nota1.value),
+  //   Number(averageCalculatorForm.nota2.value),
+  //   Number(averageCalculatorForm.nota3.value),
+  //   Number(averageCalculatorForm.nota4.value)
+  // ]
 
-  const totalGrades = grades.reduce((acc, grade) => acc + grade, 0) / 4
+  // const totalGrades = grades.reduce((acc, grade) => acc + grade, 0) / 4
+
+  const grades = Array.from(averageCalculatorForm.nota)
+
+  const gradesValue = grades.map(grade => Number(grade.value)).reduce((acc, grade) => acc + grade, 0) / 4
+
+  
 
 
   res.innerHTML = `
    <p>aluno nome: ${studentName}</p>
    <p>matéria de referência: ${subjects}</p>
    <p>ano de refêrencia: ${referenceYear}</p>
-   <p>Media de ${studentName} : ${Math.round(totalGrades)}</p>
+   <p>Media de ${studentName} : ${Math.round(gradesValue)}</p>
   `
 })
